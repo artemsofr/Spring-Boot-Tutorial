772654dcb8e463c4bec21c0938ef236b0c08b6ad
Artem Sofronov <artemsofr1985@gmail.com>
2020-05-18 18:36:17 +0300

    Creation of initial dependencies, classes and other files.

---
+++ sweater/src/main/java/com/example/sweater/GreetingController.java
@@ -0,0 +1,23 @@
+package com.example.sweater;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map <String, Object> model) {
+		model.put("name", name + "!");
+		return "greeting";
+	}
+
+	@GetMapping
+	public String index (Map <String, Object> model) {
+		model.put("some", "Welcome to sweater!!!");
+		return "index";
+	}
+}
